0409d8109c551981621d6677d664e906f9884804
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-23 09:11:45 +0900

    新規登録時のバリデーション追加（全角、全角カナ）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname           , presence: true
-  validates :last_name_kanji    , presence: true
-  validates :first_name_kanji   , presence: true
-  validates :last_name_kana     , presence: true
-  validates :first_name_kana    , presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :birthday           , presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
