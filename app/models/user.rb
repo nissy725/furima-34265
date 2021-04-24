54558c7728f2ad12468b05f30579cca5732c39b5
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-24 19:34:09 +0900

    バリデーション記述の簡素化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
-  validates :nickname           , presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name_kanji
     validates :first_name_kanji
   end
+
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
-  validates :birthday           , presence: true
-
+ 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
