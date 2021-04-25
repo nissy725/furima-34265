364861bf88be6c81e56d824d4a19a76454a89eae
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-25 15:28:59 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
 end
