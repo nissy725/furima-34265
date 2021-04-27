10056711e2554130f8378e8413f5b567de92e641
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-27 13:33:58 +0900

    ActiveHashを導入、バリデーション記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  validates :product_name, :product_description, presence: true
+  validates :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, numericality: { other_than: 1 }
 end
