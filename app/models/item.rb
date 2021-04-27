32e9d7db0e1576785f7d92d88a404f461a751c7a
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-27 20:31:13 +0900

    出品機能の実装（コントローラー、ビューの調整）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
   belongs_to :shipping_area
   belongs_to :days_to_ship
 
-  validates :image, :product_name, :product_description, presence: true
-  validates :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :product_description
+  end
+
+  with_options numericality: { other_than: 1} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
+  end
+  
 end
