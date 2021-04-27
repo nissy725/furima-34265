fe78a7d59fe0d65bd91a63062f3f93cd9d6896d2
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-27 14:23:58 +0900

    バリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,6 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :days_to_ship
 
-  validates :product_name, :product_description, presence: true
+  validates :image, :product_name, :product_description, presence: true
   validates :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, numericality: { other_than: 1 }
 end
