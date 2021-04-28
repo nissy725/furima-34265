007705480f0b13b3eb10e260328ca1633875452f
nissy725 <takanobu_725@yahoo.co.jp>
2021-04-28 15:22:36 +0900

    出品エラーメッセージの表示

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image                     {Faker::Lorem.sentence}
+    product_name              {Faker::Lorem.sentence}
+    product_description       {Faker::Lorem.sentence}
+    category_id               { '2' }
+    condition_id              { '2' }
+    shipping_charge_id        { '2' }
+    shipping_area_id          { '2' }
+    days_to_ship_id           { '2' }
+    price                     { '300' }
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '商品登録が上手く行く時' do
+      it '全ての情報が入力済みであれば商品登録できる' do
+        binding.pry
+      end
+    end
+
+    context '商品登録が上手く行かない時' do
+      it 'nicknameが空だと登録できない' do
+      end
+    end
+  end
 end
