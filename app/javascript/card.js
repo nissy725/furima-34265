9c35fd1562875566ab57b68621e15d694cfe6537
nissy725 <takanobu_725@yahoo.co.jp>
2021-05-14 14:25:56 +0900

    カード情報入力フォームから情報取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,18 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_c61b7bd1add41d6058686afc");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("購入時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_history_address[number]"),
+      exp_month: formData.get("purchase_history_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_history_address[exp_year]")}`,
+      cvc: formData.get("purchase_history_address[cvc]"),
+    };
   });
 };
 
